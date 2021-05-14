7335617ec2cec22ec7aa246f1ec08128c9a6d4b8
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-05-15 01:50:03 +0530

    Add files via upload

---
+++ build/lib/googlesearch/__init__.py
@@ -0,0 +1,10 @@
+from .GOOGLESEARCH import *
+
+
+__author__ = "Sengolda",
+__version__ = "0.1.0",
+__description__ = "A module that searches google lmao",
+__copyright__ = "Copyright {} 2021".format(__author__)
+
+def get_version():
+    print(__version__)
