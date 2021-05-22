d375daaea2debed5f92c529ad281a8ccf69005c1
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-05-22 19:51:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.1.2'
+version = '0.1.3'
 
 with open('README.md', 'r', encoding = 'utf-8') as f:
   long_description = f.read()
@@ -14,5 +14,6 @@ setup(
         url = 'https://github.com/Sengolda/googlesearch',  
         author = 'Sengolda',
         license = 'MIT', 
+        install_requires = ['urllib3>=1.26.4'],
         packages = find_packages(),
 )
