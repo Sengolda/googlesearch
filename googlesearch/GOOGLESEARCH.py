380814aba55c0ec3bbf5e0ced52a36737622bd05
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-06-13 09:28:32 +0530

    Update GOOGLESEARCH.py

--- googlesearch/GOOGLESEARCH.py
+++ googlesearch/GOOGLESEARCH.py
@@ -6,5 +6,3 @@ def search_google(query):
     x = requests.get(f"https://google.com/search?q={query}")
     soup = bs4.BeautifulSoup(x.text,"html.parser")
     return soup.text
-
-print(search_google("lion"))
